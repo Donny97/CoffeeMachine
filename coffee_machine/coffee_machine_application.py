7dc144c99e260a07ffcdf0c1e85ab82b3cdafd8f
Jayesh Patil <donny@Jayeshs-MacBook-Air.local>
2021-09-25 18:28:03 +0530

    Added skeleton file structure for the project

---
+++ coffee_machine/coffee_machine_application.py
@@ -0,0 +1,16 @@
+from typing import List, Dict
+
+class CoffeeMachine:
+    """
+    Coffee Machine main class.
+
+    Args:
+        beverage_config (Dict): Config file containing beverages configurations.
+        ingredient_config (Dict): Config file containing ingredients configurations.
+    
+    Returns:
+        None
+    """
+
+    def __init__(self, beverage_config: Dict, ingredient_config: Dict, 7) -> None:
+        
---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+NAME = 'CoffeeMachine'
+VERSION = '1.0.0'
+DESCRIPTION = 'CoffeeMachine (An OOPs project for coffee machine backend)'
+AUTHOR = 'Jayesh Patil'
+EMAIL = 'jaiyupatil@gmail.com'
+URL = '<URL>'
+REQUIREMENTS = []
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      author=AUTHOR,
+      author_email=EMAIL,
+      url=URL,
+      packages=REQUIREMENTS,
+     )
